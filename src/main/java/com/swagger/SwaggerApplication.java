8a2c8ce5a0d9dd1f50dd5a72e2fa2054a9cef27e
venkat8184 <venkatramana8184@gmail.com>
2023-05-20 11:42:36 +0530

    abc

--- src/main/java/com/swagger/SwaggerApplication.java
+++ src/main/java/com/swagger/SwaggerApplication.java
@@ -16,12 +16,14 @@ public class SwaggerApplication {
 		for(int i=0;i<5;i++){
 			System.out.println(i);
 		}
-<<<<<<< HEAD
 		if("venkat".equals("venkat")){
 			System.out.println("This is venkatramana");
 		}
 		//This is venkat ramana
 		//From master branch.
+	}
+	public static void venkat(){
+		System.out.println("Venkat");
 	}
 
 }
